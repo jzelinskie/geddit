339e56fecbf244e8136fafc5cd59a9953d687fae
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2012-09-14 23:17:42 -0400

    added a helper for http api calls; will make use later

--- const.go
+++ const.go
@@ -4,6 +4,14 @@
 
 package reddit
 
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
 const (
 	UpVote     = "1"
 	DownVote   = "-1"
@@ -11,3 +19,42 @@ const (
 )
 
 var DefaultUserAgent = "github.com/jzelinskie/reddit"
+
+func getResponse(url string, vals *url.Values, s *Session) (*bytes.Buffer, error) {
+	// Determine the HTTP action.
+	var action, finalurl string
+	if vals == nil {
+		action = "GET"
+		finalurl = url
+	} else {
+		action = "POST"
+		finalurl = url + vals.Encode()
+	}
+
+	// Create a request and add the proper headers.
+	req, err := http.NewRequest(action, finalurl, nil)
+	if err != nil {
+		return nil, err
+	}
+	if s == nil {
+		req.AddCookie(s.Cookie)
+	}
+	req.Header.Set("User-Agent", DefaultUserAgent)
+
+	// Handle the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	respbytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewBuffer(respbytes), nil
+}
