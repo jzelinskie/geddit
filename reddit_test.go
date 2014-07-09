91779948d19d7a245d20fd465041baa08af68243
Sam Cordingley <samcordingley@gmail.com>
2014-07-09 01:43:32 +0100

    add tests for captcha

--- reddit_test.go
+++ reddit_test.go
@@ -5,31 +5,58 @@ import "testing"
 func TestSubmit(t *testing.T) {
 
 	session, err := NewLoginSession(
-		"Username",
-		"Password",
+		"Algorhythmical",
+		"Parkfield.123",
 		"tester",
 	)
-
 	if err != nil {
 		t.Error(err)
 	}
 
 	subreddit, err := session.AboutSubreddit("mybottester")
-
 	if err != nil {
 		t.Error(err)
 	}
 
-	err = session.Submit(subreddit, newSubmission{"TESTING SELF", "TEST TEXT", true, true, true})
-
+	needsCaptcha, err := session.NeedsCaptcha()
 	if err != nil {
 		t.Error(err)
 	}
 
-	err = session.Submit(subreddit, newSubmission{"TESTING LINK", "https://github.com/jzelinskie/reddit", false, true, true})
+	t.Log(needsCaptcha)
 
-	if err != nil {
-		t.Error(err)
+	if needsCaptcha {
+		iden, err := session.NewCaptchaIden()
+		if err != nil {
+			t.Error(err)
+		}
+
+		_, err = session.CaptchaImage(iden)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = session.Submit(NewTextSubmission(subreddit.Name, "CAPTCHA TESTING TEXT", "TEST TEXT", true, &Captcha{iden, "test"}))
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = session.Submit(NewLinkSubmission(subreddit.Name, "CAPTCHA TESTING LINK", "https://github.com/jzelinskie/reddit", true, &Captcha{iden, "test"}))
+		if err != nil {
+			t.Error(err)
+		}
+
+	} else {
+
+		err = session.Submit(NewTextSubmission(subreddit.Name, "TESTING TEXT", "TEST TEXT", true, &Captcha{}))
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = session.Submit(NewLinkSubmission(subreddit.Name, "TESTING LINK", "https://github.com/jzelinskie/reddit", true, &Captcha{}))
+		if err != nil {
+			t.Error(err)
+		}
 	}
 
 }
