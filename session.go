87b9383764cb6d4d0566b004b1c9e7c8c090d353
gabaroar <gabaroar@gmail.com>
2015-02-19 22:24:22 -0600

    Renamed function GetSubmissions to SortedSubmissions

--- session.go
+++ session.go
@@ -57,7 +57,7 @@ func (s Session) DefaultFrontpage() ([]*Submission, error) {
 	return submissions, nil
 }
 
-func (s Session) GetSubmissions(subreddit string, sort string, config map[string]string) ([]*Submission, error) {
+func (s Session) SortedSubmissions(subreddit string, sort string, config map[string]string) ([]*Submission, error) {
 	baseUrl, err := url.Parse(fmt.Sprintf("http://www.reddit.com/r/%s/%s.json", subreddit, sort))
 	params := url.Values{}
 	for key, value := range config{
@@ -100,71 +100,6 @@ func (s Session) GetSubmissions(subreddit string, sort string, config map[string
 func (s Session) SubredditSubmissions(subreddit string) ([]*Submission, error) {
 	req := request{
 		url:       fmt.Sprintf("http://www.reddit.com/r/%s.json", subreddit),
-		useragent: s.useragent,
-	}
-	body, err := req.getResponse()
-	if err != nil {
-		return nil, err
-	}
-
-	type Response struct {
-		Data struct {
-			Children []struct {
-				Data *Submission
-			}
-		}
-	}
-
-	r := new(Response)
-	err = json.NewDecoder(body).Decode(r)
-	if err != nil {
-		return nil, err
-	}
-
-	submissions := make([]*Submission, len(r.Data.Children))
-	for i, child := range r.Data.Children {
-		submissions[i] = child.Data
-	}
-
-	return submissions, nil
-}
-
-// SortedSubmissions will return submissions from a subreddit (or homepage if "") by popularity and age
-// TODO Review this
-func (s Session) SortedSubmissions(subreddit string, popularity popularitySort, age ageSort) ([]*Submission, error) {
-	if age != DefaultAge {
-		switch popularity {
-		case NewSubmissions, RisingSubmissions, HotSubmissions:
-			return nil, fmt.Errorf("cannot sort %s by %s", popularity, age)
-		}
-	}
-
-	url := "http://reddit.com/"
-
-	if subreddit != "" {
-		url = fmt.Sprintf("%sr/%s/", url, subreddit)
-	}
-
-	if popularity != DefaultPopularity {
-		if popularity == NewSubmissions || popularity == RisingSubmissions {
-			url = fmt.Sprintf("%snew.json?sort=%s", url, popularity)
-		} else {
-			url = fmt.Sprintf("%s%s.json?sort=%s", url, popularity, popularity)
-		}
-	} else {
-		url = fmt.Sprintf("%s.json", url)
-	}
-
-	if age != DefaultAge {
-		if popularity != DefaultPopularity {
-			url = fmt.Sprintf("%s&t=%s", url, age)
-		} else {
-			url = fmt.Sprintf("%s?t=%s", url, age)
-		}
-	}
-
-	req := &request{
-		url:       url,
 		useragent: s.useragent,
 	}
 	body, err := req.getResponse()
