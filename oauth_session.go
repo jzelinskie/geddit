aafc1a6f0d617289123d1165d4f2ea242933b00c
Curtis Brandt <curtis@cpan.org>
2015-05-02 17:59:42 -0700

    Add basic coverage for OAuth API endpoints
    "Me" Endpoints:
        Me() GET api/v1/me
        MyKarama() GET api/v1/me/karma
        MyPreferences() GET api/v1/me/prefs
        MyTrophies() GET api/v1/me/trophies
        MyFriends() GET api/v1/me/friends
    
    User Endpoints:
        AboutRedditor() GET user/<user>/about
        UserTrophies() GET api/v1/user/<user>/trophies
    
    Subreddit Endpoints:
        AboutSubreddit() GET r/<subreddit>/about
        Comments() GET comments/<subreddit>
        Submit() POST api/submit
        Delete() POST api/del
        SubredditSubmissions() GET r/<subreddit>
        FrontPage() GET

--- oauth_session.go
+++ oauth_session.go
@@ -10,7 +10,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
+	"github.com/google/go-querystring/query"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
@@ -60,20 +64,19 @@ func NewOAuthSession(clientID, clientSecret, useragent string, limit bool) (*OAu
 	return s, nil
 }
 
-func (s OAuthSession) getBody(url string, d interface{}) error {
-	req, err := http.NewRequest("GET", url, nil)
+func (o *OAuthSession) getBody(link string, d interface{}) error {
+	req, err := http.NewRequest("GET", link, nil)
 	if err != nil {
 		return err
 	}
 
 	// This is needed to avoid rate limits
-	req.Header.Set("User-Agent", s.UserAgent)
+	req.Header.Set("User-Agent", o.UserAgent)
 
-	resp, err := s.Client.Do(req)
+	resp, err := o.Client.Do(req)
 	if err != nil {
 		return err
 	}
-	fmt.Println(req.Header.Get("User-Agent"))
 	defer resp.Body.Close()
 
 	// DEBUG
@@ -86,4 +89,259 @@ func (s OAuthSession) getBody(url string, d interface{}) error {
 	}
 
 	return nil
+}
+
+func (o *OAuthSession) Me() (*Redditor, error) {
+	r := &Redditor{}
+	err := o.getBody("https://oauth.reddit.com/api/v1/me", r)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func (o *OAuthSession) MyKarma() ([]Karma, error) {
+	type karma struct {
+		Data []Karma
+	}
+	k := &karma{}
+	err := o.getBody("https://oauth.reddit.com/api/v1/me/karma", k)
+	if err != nil {
+		return nil, err
+	}
+	return k.Data, nil
+}
+
+func (o *OAuthSession) MyPreferences() (*Preferences, error) {
+	p := &Preferences{}
+	err := o.getBody("https://oauth.reddit.com/api/v1/me/prefs", p)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (o *OAuthSession) MyFriends() ([]Friend, error) {
+	type friends struct {
+		Data struct {
+			Children []Friend
+		}
+	}
+	f := &friends{}
+	err := o.getBody("https://oauth.reddit.com/api/v1/me/friends", f)
+	if err != nil {
+		return nil, err
+	}
+	return f.Data.Children, nil
+}
+
+func (o *OAuthSession) MyTrophies() ([]*Trophy, error) {
+	type trophyData struct {
+		Data struct {
+			Trophies []struct {
+				Data Trophy
+			}
+		}
+	}
+
+	t := &trophyData{}
+	err := o.getBody("https://oauth.reddit.com/api/v1/me/trophies", t)
+	if err != nil {
+		return nil, err
+	}
+
+	var myTrophies []*Trophy
+	for _, trophy := range t.Data.Trophies {
+		myTrophies = append(myTrophies, &trophy.Data)
+	}
+	return myTrophies, nil
+}
+
+// AboutRedditor returns a Redditor for the given username using OAuth.
+func (o *OAuthSession) AboutRedditor(user string) (*Redditor, error) {
+	type redditor struct {
+		Data Redditor
+	}
+	r := &redditor{}
+	link := fmt.Sprintf("https://oauth.reddit.com/user/%s/about", user)
+
+	err := o.getBody(link, r)
+	if err != nil {
+		return nil, err
+	}
+	return &r.Data, nil
+}
+
+func (o *OAuthSession) UserTrophies(user string) ([]*Trophy, error) {
+	type trophyData struct {
+		Data struct {
+			Trophies []struct {
+				Data Trophy
+			}
+		}
+	}
+
+	t := &trophyData{}
+	url := fmt.Sprintf("https://oauth.reddit.com/api/v1/user/%s/trophies", user)
+	err := o.getBody(url, t)
+	if err != nil {
+		return nil, err
+	}
+
+	var trophies []*Trophy
+	for _, trophy := range t.Data.Trophies {
+		trophies = append(trophies, &trophy.Data)
+	}
+	return trophies, nil
+}
+
+// AboutSubreddit returns a subreddit for the given subreddit name using OAuth.
+func (o *OAuthSession) AboutSubreddit(name string) (*Subreddit, error) {
+	type subreddit struct {
+		Data Subreddit
+	}
+	sr := &subreddit{}
+	link := fmt.Sprintf("https://oauth.reddit.com/r/%s/about", name)
+
+	err := o.getBody(link, sr)
+	if err != nil {
+		return nil, err
+	}
+	return &sr.Data, nil
+}
+
+// Comments returns the comments for a given Submission using OAuth.
+func (o *OAuthSession) Comments(h *Submission) ([]*Comment, error) {
+	var c interface{}
+	link := fmt.Sprintf("https://oauth.reddit.com/comments/%s", h.ID)
+	err := o.getBody(link, &c)
+	if err != nil {
+		return nil, err
+	}
+	helper := new(helper)
+	helper.buildComments(c)
+	return helper.comments, nil
+}
+
+func (o *OAuthSession) postBody(link string, form url.Values, d interface{}) error {
+	req, err := http.NewRequest("POST", link, strings.NewReader(form.Encode()))
+	if err != nil {
+		return err
+	}
+
+	// This is needed to avoid rate limits
+	req.Header.Set("User-Agent", o.UserAgent)
+
+	// POST form provided
+	req.PostForm = form
+
+	resp, err := o.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	// DEBUG
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("***DEBUG***\nRequest Body: %s\n***DEBUG***\n\n", body)
+
+	// The caller may want JSON decoded, or this could just be an update/delete request.
+	if d != nil {
+		err = json.Unmarshal(body, d)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// SubmitLink accepts a NewSubmission type and submits a new link using OAuth.
+// Returns a Submission type.
+func (o *OAuthSession) Submit(ns *NewSubmission) (*Submission, error) {
+
+	// Build form for POST request.
+	v := url.Values{
+		"title":       {ns.Title},
+		"url":         {ns.Content},
+		"text":        {ns.Content},
+		"sr":          {ns.Subreddit},
+		"sendreplies": {strconv.FormatBool(ns.SendReplies)},
+		"resubmit":    {strconv.FormatBool(ns.Resubmit)},
+		"api_type":    {"json"},
+		// TODO implement captchas for OAuth types
+		//"captcha":     {ns.Captcha.Response},
+		//"iden":        {ns.Captcha.Iden},
+	}
+	if ns.Self {
+		v.Add("kind", "self")
+	} else {
+		v.Add("kind", "link")
+	}
+
+	type submission struct {
+		Json struct {
+			Errors [][]string
+			Data   Submission
+		}
+	}
+	submit := &submission{}
+
+	err := o.postBody("https://oauth.reddit.com/api/submit", v, submit)
+	if err != nil {
+		return nil, err
+	}
+	// TODO check s.Errors and do something useful?
+	return &submit.Json.Data, nil
+}
+
+// Delete deletes a link or comment using the given full name ID.
+func (o *OAuthSession) Delete(d Deleter) error {
+	// Build form for POST request.
+	v := url.Values{}
+	v.Add("id", d.deleteID())
+
+	return o.postBody("https://oauth.reddit.com/api/del", v, nil)
+}
+
+// SubredditSubmissions returns the submissions on the given subreddit using OAuth.
+func (o *OAuthSession) SubredditSubmissions(subreddit string, sort popularitySort, params ListingOptions) ([]*Submission, error) {
+	v, err := query.Values(params)
+	if err != nil {
+		return nil, err
+	}
+
+	baseUrl := "https://oauth.reddit.com"
+
+	// If subbreddit given, add to URL
+	if subreddit != "" {
+		baseUrl += "/r/" + subreddit
+	}
+
+	redditURL := fmt.Sprintf(baseUrl+"/%s.json?%s", sort, v.Encode())
+
+	type Response struct {
+		Data struct {
+			Children []struct {
+				Data *Submission
+			}
+		}
+	}
+
+	r := new(Response)
+	err = o.getBody(redditURL, r)
+	if err != nil {
+		return nil, err
+	}
+
+	submissions := make([]*Submission, len(r.Data.Children))
+	for i, child := range r.Data.Children {
+		submissions[i] = child.Data
+	}
+
+	return submissions, nil
+}
+
+// Frontpage returns the submissions on the default reddit frontpage using OAuth.
+func (o *OAuthSession) Frontpage(sort popularitySort, params ListingOptions) ([]*Submission, error) {
+	return o.SubredditSubmissions("", sort, params)
 }
--- redditor.go
+++ redditor.go
@@ -9,15 +9,55 @@ import (
 )
 
 type Redditor struct {
-	ID           string  `json:"id"`
-	Name         string  `json:"name"`
-	LinkKarma    int     `json:"link_karma"`
-	CommentKarma int     `json:"comment_karma"`
-	Created      float64 `json:"created_utc"`
-	Gold         bool    `json:"is_gold"`
-	Mod          bool    `json:"is_mod"`
-	Mail         *bool   `json:"has_mail"`
-	ModMail      *bool   `json:"has_mod_mail"`
+	ID      string  `json:"id"`
+	Name    string  `json:"name"`
+	Created float32 `json:"created_utc"`
+	Gold    bool    `json:"is_gold"`
+	Mod     bool    `json:"is_mod"`
+	Mail    *bool   `json:"has_mail"`
+	ModMail *bool   `json:"has_mod_mail"`
+	Karma
+}
+
+type Preferences struct {
+	Research               bool   `json:"research"`
+	ShowStylesheets        bool   `json:"show_stylesheets"`
+	ShowLinkFlair          bool   `json:"show_link_flair"`
+	ShowTrending           bool   `json:"show_trending"`
+	PrivateFeeds           bool   `json:"private_feeds"`
+	IgnoreSuggestedSort    bool   `json:"ignore_suggested_sort"`
+	Media                  string `json:"media"`
+	ClickGadget            bool   `json:"clickgadget"`
+	LabelNSFW              bool   `json:"label_nsfw"`
+	Over18                 bool   `json:"over_18"`
+	EmailMessages          bool   `json:"email_messages"`
+	HighlightControversial bool   `json:"highlight_controversial"`
+	ForceHTTPS             bool   `json:"force_https"`
+	Language               string `json:"lang"`
+	HideFromRobots         bool   `json:"hide_from_robots"`
+	PublicVotes            bool   `json:"public_votes"`
+	ShowFlair              bool   `json:"show_flair"`
+	HideAds                bool   `json:"hide_ads"`
+	Beta                   bool   `json:"beta"`
+	NewWindow              bool   `json:"newwindow"`
+	LegacySearch           bool   `json:"legacy_search"`
+}
+
+type Friend struct {
+	Date float32 `json:"date"`
+	Name string  `json:"name"`
+	ID   string  `json:"id"`
+}
+
+type Karma struct {
+	CommentKarma int `json:"comment_karma"`
+	LinkKarma    int `json:"link_karma"`
+}
+
+type Trophy struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Icon        string `json:"icon_70"`
 }
 
 // String returns the string representation of a reddit user.
