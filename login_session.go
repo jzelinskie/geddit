c6e931608408184cd431f9c6c586534ba491ddc1
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2016-11-14 16:52:23 -0500

    fix name in package docs

--- login_session.go
+++ login_session.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package reddit implements an abstraction for the reddit.com API.
+// Package geddit implements an abstraction for the reddit.com API.
 package geddit
 
 import (
